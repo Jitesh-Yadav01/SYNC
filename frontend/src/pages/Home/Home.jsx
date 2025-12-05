69b0b400cf4eb615acadfec4823c1779caf078d5
SajalRawat <71058080+SajalRawat@users.noreply.github.com>
2025-12-05 09:47:04 +0530

    Optimized Mobile View
    thanks to designer @Abhinav01-create

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Loader from '../../components/Loader/Loader'
 import './Home.css'
 import Logocollections from "../../assets/clublogos/clublogos.svg"
+import Synclogo from "../../assets/clublogos/synclogo.svg"
 
 export default function Home() {
 	/*
@@ -20,13 +21,16 @@ export default function Home() {
 	*/
 	return (
 		<section id="home" className="home-hero">
+			<div className="syncLogoDiv">
+				<img src={Synclogo} alt="" />
+			</div>
 			<div className="page-head flex  font-[Poppins]">
 				<div className='hero-div' >
 					<h1 className="blue-hero hero">A Place</h1>
-					<h1 className="blue-hero hero">Where Every</h1>
+					<h1 className="blue-hero hero"><span className='text-red-500'>Where Every</span></h1>
 					<h1 className="blue-hero hero">Club Finds a</h1>
 					<h1 className="gray-hero hero">Home</h1>
-					<div>
+					<div className='secondary-hero-div'>
 						<p className='line1'>
 							A one-stop solution to all the announcement and 
 						</p>
@@ -40,12 +44,12 @@ export default function Home() {
 
 						</p>
 						<div className='flex hero-btn-div'>
-							<button className='hero-btn'>ANNOUNCEMENTS</button>
-							<button className='hero-btn'>ALL CLUBS</button>
+							<button className='hero-btn bg-blue-300 font-medium'>ANNOUNCEMENTS</button>
+							<button className='hero-btn font-medium'>ALL CLUBS</button>
 						</div>
 					</div>
 				</div>
-				<div>
+				<div className='clubCollectionDiv'>
 					<img className='w-[70vw] clubCollection' src={Logocollections} alt="" />
 				</div>
 			</div>
